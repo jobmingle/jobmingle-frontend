f44a5564c84624348571637aec72c06fd8db3f58
godspowerufot <ufotgodspower8@gmail.com>
2024-07-09 18:33:27 +0100

    fix the FAQ issue , next code optimization

--- app/component/ui/FAQSection.tsx
+++ app/component/ui/FAQSection.tsx
@@ -4,20 +4,26 @@ import { useState } from "react";
 import Image from "next/image";
 import LaptopPic from "@/public/jobSearch.jpg";
 
-// declaring type
-type Props = {
-  question: String;
+// Declaring type for FAQ items
+type FAQItemType = {
+  question: string;
   answer: string;
 };
+
 const FAQSection = () => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const handleToggle = (index: number) => {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
-    <div className=" flex justify-around items-center max-w-screen-xl mt-20 mx-auto px-5 bg-white min-h-screen">
+    <div className="flex justify-around items-center max-w-screen-xl mt-20 mx-auto px-5 bg-white min-h-screen">
       <div className="relative group">
         <Image
           src={LaptopPic}
-          // data-aos="fade-down-right"
           alt="ImageOne"
-          className="max-md:hidden   w-[500px] h-[500px] shadow rounded-2xl object-cover img1"
+          className="max-md:hidden w-[500px] h-[500px] shadow rounded-2xl object-cover img1"
         />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-2xl">
           <p className="text-white text-center px-4 text-2xl">
@@ -36,8 +42,11 @@ const FAQSection = () => {
           {faqItems.map((item, index) => (
             <FAQItem
               key={index}
+              index={index}
               question={item.question}
               answer={item.answer}
+              isOpen={openIndex === index}
+              onToggle={handleToggle}
             />
           ))}
         </div>
@@ -45,16 +54,28 @@ const FAQSection = () => {
     </div>
   );
 };
+//component
 
-const FAQItem = ({ question, answer }: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+type FAQItemProps = {
+  index: number;
+  question: string;
+  answer: string;
+  isOpen: boolean;
+  onToggle: (index: number) => void;
+};
 
+const FAQItem = ({
+  index,
+  question,
+  answer,
+  isOpen,
+  onToggle,
+}: FAQItemProps) => {
   return (
     <div className="py-5">
-      <details
+      <div
         className={`group ${isOpen ? "open" : ""}`}
-        open={isOpen}
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => onToggle(index)}
       >
         <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
           <span>{question}</span>
@@ -74,19 +95,22 @@ const FAQItem = ({ question, answer }: Props) => {
             </svg>
           </span>
         </summary>
-        <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
-          {answer}
-        </p>
-      </details>
+        {/* if it opens close else opene it  */}
+        {isOpen && (
+          <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
+            {answer}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
 
-const faqItems = [
+const faqItems: FAQItemType[] = [
   {
     question: "What is JobMingle?",
     answer:
-      "JobMingle is an innovative ed-tech platform and remote job recruitment website. We empower individuals seeking to transition to a new career by equipping them with the right skills and providing access to numerous remote job opportunities across the country. We assist employers in finding the remote talent they desire to grow their company and connect job seekers to the jobs they need to succeed",
+      "JobMingle is an innovative ed-tech platform and remote job recruitment website. We empower individuals seeking to transition to a new career by equipping them with the right skills and providing access to numerous remote job opportunities across the country. We assist employers in finding the remote talent they desire to grow their company and connect job seekers to the jobs they need to succeed.",
   },
   {
     question: "Do I need a laptop before I can visit Jobmingle.co?",
@@ -96,17 +120,17 @@ const faqItems = [
   {
     question: "Is there a mobile app available for JobMingle?",
     answer:
-      "No. But we are currently working on it. We will notify you when it is available for download onthe Play Store and Apple Store.",
+      "No. But we are currently working on it. We will notify you when it is available for download on the Play Store and Apple Store.",
   },
   {
     question: "What kind of courses are available on JobMingle?",
     answer:
-      "We offer a wide range of courses across multiple industries. Which means we are not specific to a particular industry",
+      "We offer a wide range of courses across multiple industries. Which means we are not specific to a particular industry.",
   },
   {
     question: "Is it only remote jobs that are listed on JobMingle?",
     answer:
-      "Yes. However, we also list hybrid jobs that require you to go to work a few times a week",
+      "Yes. However, we also list hybrid jobs that require you to go to work a few times a week.",
   },
   {
     question: "Can I advertise with JobMingle?",
