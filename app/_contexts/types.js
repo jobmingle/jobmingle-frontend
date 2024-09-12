e65e154c641bffa2963ea57b58e1a7cdd81f9c6f
sundaygbenga <sunflizasher@gmail.com>
2024-09-12 03:48:58 +0100

    Pushed recent changes before merge

---
+++ app/_contexts/types.js
@@ -0,0 +1,13 @@
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const CONFIRMATION_SUCCESS = "CONFIRMATION_SUCCESS";
+export const CONFIRMATION_FAIL = "CONFIRMATION_FAIL";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const RESET_SUCCESS = "RESET_SUCCESS";
+export const RESET_FAIL = "RESET_FAIL";
+export const LOGOUT = "LOGOUT";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const LOADING = "LOADING";
